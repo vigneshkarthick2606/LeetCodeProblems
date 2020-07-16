f03e68320e87d453d883cdb3893656047db045e4
vigneshkarthick2606 <60441471+vigneshkarthick2606@users.noreply.github.com>
2020-07-16 08:23:52 +0530

    Create SortColors.java

---
+++ Arrays/SortColors.java
@@ -0,0 +1,26 @@
+Sort Colors(Dutch National Flag alg)
+
+    public void sortColors(int[] nums) {
+        
+      int low = 0,mid = 0, high;
+      high = nums.length -1;
+      while(mid<=high){  // impt <=
+          if(nums[mid] == 0){
+              swap(low,mid, nums);
+              low++;
+              mid++;
+          }else if(nums[mid] == 1){
+              mid++;
+          }else{
+              swap(mid,high, nums);
+              high--;
+          }  
+      }
+      
+    }
+    
+    public void swap(int l, int r, int[] nums){
+        int temp = nums[l];
+        nums[l]=nums[r];
+        nums[r]=temp;
+    }
